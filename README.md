8ab52ca735e8746da0701ecc7af9e90fbe3835ee
Jacky Chow <jckchow@ucalgary.ca>
2017-12-23 07:19:59 -0700

    First working copy of bundle adjustment. Results match FEMBUN.

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Date: December 21, 2017
 1. Run cmake with destination set to a new folder you create called "build"
 2. Navigate to the build folder and run "make" to compile
 3. Call ./bundleAdjustment to run the code
+
+Note: If the y-axis is set to -1 it means we have a LHS and currently the code doesn't do anything with it. The user need to manually do the following 3 things:
+
+- Change image observation y to -y
+- Change yp to -yp
+- Change p2 to -p2 (tangential component of the DLD)
--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,45 @@ struct constantConstraint {
   const double weight_;
 };
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Pseudo observation of XYZ
+/// Input:    X       - Some constant value
+///           Y
+///           Z
+///           XStdDev - weight to constrain it
+///           YStdDev
+///           ZStdDev
+/// Unknowns: XYZ     - the object space coordinate
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct constrainPoint {
+  
+  constrainPoint(double X, double Y, double Z, double XStdDev, double YStdDev, double ZStdDev)
+        : X_(X), Y_(Y), Z_(Z), XStdDev_(XStdDev), YStdDev_(YStdDev), ZStdDev_(ZStdDev){}
+
+  template <typename T>
+  // unknown parameters followed by the output residual
+  bool operator()(const T* const XYZ, T* residual) const {
+
+  residual[0] = XYZ[0] - T(X_);
+  residual[1] = XYZ[1] - T(Y_);
+  residual[2] = XYZ[2] - T(Z_);
+  residual[0] /= T(XStdDev_);
+  residual[1] /= T(YStdDev_);
+  residual[2] /= T(ZStdDev_);
+
+  return true;
+  }
+
+ private:
+  // what we want to constraint the point to
+  const double X_;
+  const double Y_;
+  const double Z_;
+  const double XStdDev_;
+  const double YStdDev_;
+  const double ZStdDev_;
+
+};
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Collinearity Equation
@@ -78,17 +117,31 @@ struct collinearity {
   T YTemp = r21 * ( XYZ[0] - EOP[3] ) + r22 * ( XYZ[1] - EOP[4] ) + r23 * ( XYZ[2] - EOP[5] );
   T ZTemp = r31 * ( XYZ[0] - EOP[3] ) + r32 * ( XYZ[1] - EOP[4] ) + r33 * ( XYZ[2] - EOP[5] );
 
+//   std::cout<<"XYZ[0], XYZ[1], XYZ[2]: "<<XYZ[0]<<", "<<XYZ[1]<<", "<<XYZ[2]<<std::endl;
+//   std::cout<<"EOP[3], EOP[4], EOP[5]: "<<EOP[3]<<", "<<EOP[4]<<", "<<EOP[5]<<std::endl;
+//   std::cout<<"XTemp, YTemp, ZTemp: "<<XTemp<<", "<<YTemp<<", "<<ZTemp<<std::endl;
+
+//   sleep(10000000000);        
+
   // collinearity condition
   T x = -IOP[2] * XTemp / ZTemp;
   T y = -IOP[2] * YTemp / ZTemp;
+
+//   std::cout<<"x, y: "<<x+T(xp_)<<", "<<y+T(yp_)<<std::endl;
+//   std::cout<<"x_obs, y_obs: "<<T(x_)<<", "<<T(y_)<<std::endl;
+
 
   // camera correction model AP = a1, a2, k1, k2, k3, p1, p2, ...
   T x_bar = T(x_) - T(xp_);
   T y_bar = T(y_) - T(yp_);
   T r = sqrt(x_bar*x_bar + y_bar*y_bar);
 
-  T delta_x = x_bar*(AP[2]*pow(r,2.0)+AP[3]*pow(r,4.0)+AP[4]*pow(r,6.0)) + AP[5]*(pow(r,2.0)+2*pow(x_bar,2.0))+2.0*AP[6]*x_bar*y_bar + AP[0]*x_bar+AP[1]*y_bar;
-  T delta_y = y_bar*(AP[2]*pow(r,2.0)+AP[3]*pow(r,4.0)+AP[4]*pow(r,6.0)) + AP[6]*(pow(r,2.0)+2*pow(y_bar,2.0))+2.0*AP[5]*x_bar*y_bar;
+//   T delta_x = x_bar*(AP[2]*pow(r,2.0)+AP[3]*pow(r,4.0)+AP[4]*pow(r,6.0)) + AP[5]*(pow(r,2.0)+T(2.0)*pow(x_bar,2.0))+T(2.0)*AP[6]*x_bar*y_bar + AP[0]*x_bar+AP[1]*y_bar;
+//   T delta_y = y_bar*(AP[2]*pow(r,2.0)+AP[3]*pow(r,4.0)+AP[4]*pow(r,6.0)) + AP[6]*(pow(r,2.0)+T(2.0)*pow(y_bar,2.0))+T(2.0)*AP[5]*x_bar*y_bar;
+
+  T delta_x = x_bar*(AP[2]*r*r+AP[3]*r*r*r*r+AP[4]*r*r*r*r*r*r) + AP[5]*(r*r+T(2.0)*x_bar*x_bar)+T(2.0)*AP[6]*x_bar*y_bar + AP[0]*x_bar+AP[1]*y_bar;
+  T delta_y = y_bar*(AP[2]*r*r+AP[3]*r*r*r*r+AP[4]*r*r*r*r*r*r) + AP[6]*(r*r+T(2.0)*y_bar*y_bar)+T(2.0)*AP[5]*x_bar*y_bar;
+
 
   T x_true = x + IOP[0] + delta_x;
   T y_true = y + IOP[1] + delta_y;
@@ -97,8 +150,10 @@ struct collinearity {
   residual[0] = x_true - T(x_); // x-residual
   residual[1] = y_true - T(y_); // y-residual    
 
-  residual[0] *= T(xStdDev_);
-  residual[1] *= T(yStdDev_);
+//   std::cout<<"x_diff, y_diff: "<<residual[0]<<", "<<residual[1]<<std::endl;
+
+  residual[0] /= T(xStdDev_);
+  residual[1] /= T(yStdDev_);
 
   return true;
   }
@@ -188,6 +243,12 @@ int main(int argc, char** argv) {
         double c3, c4, c5, c6, c7, c8;
         inp  >> c1 >> c2 >> c3 >> c4 >> c5 >> c6 >> c7 >> c8;
 
+        //convert angles to radians
+        c6 *= PI / 180.0;
+        c7 *= PI / 180.0;
+        c8 *= PI / 180.0;
+
+
         eopStation.push_back(c1);
         eopCamera.push_back(c2);
         eopXo.push_back(c3);
@@ -269,8 +330,8 @@ int main(int argc, char** argv) {
 
         std::vector<double> tempAP;
         tempAP.resize(7);
-        tempAP[0] = c10;
-        tempAP[1] = c11;
+        tempAP[0] = c10; //a1
+        tempAP[1] = c11; //a2
         tempAP[2] = c12;
         tempAP[3] = c13;
         tempAP[4] = c14;
@@ -354,6 +415,11 @@ int main(int argc, char** argv) {
     std::sort (xyzTargetID.begin(), xyzTargetID.end()); // must sort before the following unique function works
     xyzTargetID.erase(std::unique(xyzTargetID.begin(), xyzTargetID.end()), xyzTargetID.end());
     std::cout << "    Number of unique Targets read: "<< xyzTargetID.size() << std::endl;
+    std::cout << "      Data: " <<std::endl;
+    for (int i = 0; i < xyzTarget.size(); i++)
+    {
+        std::cout<<xyzTarget[i]<<" \t "<<xyzX[i]<<" \t "<<xyzY[i]<<" \t "<<xyzZ[i]<<" \t "<<xyzXStdDev[i]<<" \t "<<xyzYStdDev[i]<<" \t "<<xyzZStdDev[i]<<std::endl;
+    }
 
     PyRun_SimpleString("print 'Done reading data:', round(TIME.clock()-t0, 3), 's' ");
 
@@ -369,13 +435,54 @@ int main(int argc, char** argv) {
     // //////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // /// Set up cost functions
     // //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // PyRun_SimpleString("t0 = TIME.clock()");        
-    // PyRun_SimpleString("print 'Start building Ceres-Solver cost functions' ");     
+    PyRun_SimpleString("t0 = TIME.clock()");        
+    PyRun_SimpleString("print 'Start building Ceres-Solver cost functions' ");     
    
-    // ceres::Problem problem;
-    // ceres::LossFunction* loss = NULL;
-    // // loss = new ceres::HuberLoss(1.0);
-    // // loss = new ceres::CauchyLoss(0.5);
+    ceres::Problem problem;
+    ceres::LossFunction* loss = NULL;
+    // loss = new ceres::HuberLoss(1.0);
+    // loss = new ceres::CauchyLoss(0.5);
+
+    for(int n = 0; n < imageX.size(); n++) // loop through all observations
+    {
+         std::vector<int>::iterator it;
+         it = std::find(xyzTarget.begin(), xyzTarget.end(), imageTarget[n]);
+         int indexPoint = std::distance(xyzTarget.begin(),it);
+         // std::cout<<"indexPoint: "<<indexPoint<<", ID: "<< imageTarget[n]<<std::endl;
+
+         it = std::find(eopStation.begin(), eopStation.end(), imageStation[n]);
+         int indexPose = std::distance(eopStation.begin(),it);
+         // std::cout<<"index: "<<indexPose<<", ID: "<< imageStation[n]<<std::endl;
+
+         it = std::find(iopCamera.begin(), iopCamera.end(), eopCamera[indexPose]);
+         int indexSensor = std::distance(iopCamera.begin(),it);
+         // std::cout<<"index: "<<indexSensor<<", ID: "<< eopCamera[indexPose]<<std::endl;   
+
+        //  std::cout<<"EOP: "<< EOP[indexPose][3] <<", " << EOP[indexPose][4] <<", " << EOP[indexPose][5]  <<std::endl;
+        //  std::cout<<"XYZ: "<< XYZ[indexPoint][0] <<", " << XYZ[indexPoint][1] <<", " << XYZ[indexPoint][2]  <<std::endl;
+
+        ceres::CostFunction* cost_function =
+            new ceres::AutoDiffCostFunction<collinearity, 2, 6, 3, 3, 7>(
+                new collinearity(imageX[n],imageY[n],imageXStdDev[n], imageYStdDev[n],iopXp[indexSensor],iopYp[indexSensor]));
+        problem.AddResidualBlock(cost_function, NULL, &EOP[indexPose][0], &XYZ[indexPoint][0], &IOP[indexSensor][0], &AP[indexSensor][0]);  
+
+        problem.SetParameterBlockConstant(&IOP[indexSensor][0]);
+        problem.SetParameterBlockConstant(&AP[indexSensor][0]);
+ 
+    }
+ 
+    // define the datum by pseduo observations of the positions
+    if(true)
+    {
+        for(int n = 0; n < xyzTarget.size(); n++)
+        {
+            ceres::CostFunction* cost_function =
+                new ceres::AutoDiffCostFunction<constrainPoint, 3, 3>(
+                    new constrainPoint(xyzX[n], xyzY[n], xyzZ[n], xyzXStdDev[n], xyzYStdDev[n], xyzZStdDev[n]));
+            problem.AddResidualBlock(cost_function, NULL, &XYZ[n][0]);
+        }
+    }
+
 
     // if(true)
     // {
@@ -391,51 +498,95 @@ int main(int argc, char** argv) {
     // for(int n = 0; n < X.size(); n++)
     // std::cout<<"before X: "<<X[n]<<std::endl;
 
-    // ceres::Solver::Options options;
-    // options.max_num_iterations = 50;
+    ceres::Solver::Options options;
+    options.max_num_iterations = 50;
     // options.linear_solver_type = ceres::SPARSE_NORMAL_CHOLESKY; // sparse solver
-    // options.minimizer_progress_to_stdout = true;
-	// options.max_lm_diagonal = 1.0E-150; // force it behave like a Gauss-Newton update
-    // options.min_lm_diagonal = 1.0E-150;
-    // ceres::Solver::Summary summary;
-    // ceres::Solve(options, &problem, &summary);
-    // std::cout << summary.BriefReport() << "\n";
-    // std::cout << summary.FullReport() << "\n";
+    options.linear_solver_type = ceres::DENSE_QR;
+    options.minimizer_progress_to_stdout = true;
+	options.max_lm_diagonal = 1.0E-150; // force it behave like a Gauss-Newton update
+    options.min_lm_diagonal = 1.0E-150;
+    // options.function_tolerance = 1.0E-20;
+    ceres::Solver::Summary summary;
+    ceres::Solve(options, &problem, &summary);
+    std::cout << summary.BriefReport() << "\n";
+    std::cout << summary.FullReport() << "\n";
 
-    // for(int n = 0; n < X.size(); n++)
-    // std::cout<<"final X: "<<X[n]<<std::endl;
+    std::cout<<"After XYZ"<<std::endl;
+    for (int n = 0; n < XYZ.size(); n++)
+    {
+        std::cout<<xyzTarget[n]<<": "<<XYZ[n][0]<<", "<<XYZ[n][1]<<", "<<XYZ[n][2]<<std::endl;
+    }
+    std::cout<<"After EOP"<<std::endl;
+    for (int n = 0; n < EOP.size(); n++)
+    {
+        std::cout<<eopStation[n]<<": "<<EOP[n][0]*180.0/PI<<", "<<EOP[n][1]*180.0/PI<<", "<<EOP[n][2]*180.0/PI<<", "<<EOP[n][3]<<", "<<EOP[n][4]<<", "<<EOP[n][5]<<std::endl;
+    }
+    std::cout<<"After IOP"<<std::endl;
+    for (int n = 0; n < iopCamera.size(); n++)
+    {
+        std::cout<<iopCamera[n]<<": "<<IOP[n][0]<<", "<<IOP[n][1]<<", "<<IOP[n][2]<<std::endl;
+    }
+    std::cout<<"After AP"<<std::endl;
+    for (int n = 0; n < iopCamera.size(); n++)
+    {
+        std::cout<<iopCamera[n]<<": "<<AP[n][0]<<", "<<AP[n][1]<<", "<<AP[n][2]<<AP[n][3]<<", "<<AP[n][4]<<", "<<AP[n][5]<<", "<<AP[n][6]<<std::endl;
+    }
 
-    // ceres::Covariance::Options covarianceOoptions;
-    // ceres::Covariance covariance(covarianceOoptions);
+    ceres::Covariance::Options covarianceOoptions;
+    ceres::Covariance covariance(covarianceOoptions);
 
-    // std::vector<std::pair<const double*, const double*> > covariance_blocks;
-    // for(int i = 0; i < X.size(); i++)
-    //     for(int j = 0; j < X.size(); j++)
-    //         covariance_blocks.push_back(std::make_pair(&X[i], &X[j]));
+    std::vector<std::pair<const double*, const double*> > covariance_blocks;
+    // for(int i = 0; i < XYZ.size(); i++)
+    //     for(int j = 0; j < XYZ.size(); j++)
+            // covariance_blocks.push_back(std::make_pair(&XYZ[i][0], &XYZ[j][0]));
+    covariance_blocks.push_back(std::make_pair(&XYZ[0][0], &XYZ[0][0]));
+    covariance_blocks.push_back(std::make_pair(&XYZ[1][0], &XYZ[1][0]));
+    covariance_blocks.push_back(std::make_pair(&XYZ[2][0], &XYZ[2][0]));
 
-    // covariance.Compute(covariance_blocks, &problem);
+    covariance.Compute(covariance_blocks, &problem);
 
     // //double covariance_X[X.size() * X.size()];
-    // Eigen::Matrix<double,3,3> covariance_X;
+    // Eigen::MatrixXd covariance_X(XYZ.size(), 3);
     // // covariance_X.resize(X.size() * X.size());
     // //covariance.GetCovarianceBlock(&X[0], &X[0], covariance_X.data());
 
-    // for(int i = 0; i < X.size(); i++)
-    //     for(int j = 0; j < X.size(); j++)
+    // for(int i = 0; i < XYZ.size(); i++)
+    //     for(int j = 0; j < XYZ.size(); j++)
     //     {
-    //         double temp[1];
-    //         temp[0] = 0.0;
-    //         covariance.GetCovarianceBlock(&X[i], &X[j], temp);
-    //         std::cout<<"std: "<<sqrt(temp[0])<<std::endl;
-    //         covariance_X(i,j) = temp[0];
+    //         double temp[9];
+    //         //std::cout<<"std: "<<sqrt(temp[0])<<std::endl;
+    //         covariance_X(i,0) = temp[0];
     //     }
 
-    // // std::cout<<"std: "<<sqrt(covariance_X[0])<<", size: "<<covariance_X.size()<<std::endl;
-    // // std::cout<<"std: "<<sqrt(covariance_X[4])<<", size: "<<covariance_X.size()<<std::endl;
-    //  std::cout << "Variance: " << covariance_X.diagonal() << std::endl; 
 
-    //  std::cout<<"covariance matrix: "<<std::endl;
-    //  std::cout<<covariance_X<<std::endl;
+    Eigen::MatrixXd covariance_X(3, 3);
+    covariance.GetCovarianceBlock(&XYZ[0][0], &XYZ[0][0], covariance_X.data());
+     std::cout << "Variance: " << covariance_X.diagonal() << std::endl; 
+     std::cout<<"covariance matrix: "<<std::endl;
+     std::cout<<covariance_X<<std::endl;
+
+    covariance.GetCovarianceBlock(&XYZ[1][0], &XYZ[1][0], covariance_X.data());
+     std::cout << "Variance: " << covariance_X.diagonal() << std::endl; 
+     std::cout<<"covariance matrix: "<<std::endl;
+     std::cout<<covariance_X<<std::endl;
+
+    covariance.GetCovarianceBlock(&XYZ[2][0], &XYZ[2][0], covariance_X.data());
+     std::cout << "Variance: " << covariance_X.diagonal() << std::endl; 
+     std::cout<<"covariance matrix: "<<std::endl;
+     std::cout<<covariance_X<<std::endl;
+
+    std::vector<double> residuals;
+    double cost = 0.0;
+    problem.Evaluate(ceres::Problem::EvaluateOptions(), &cost, &residuals, NULL, NULL);
+
+    Eigen::MatrixXd imageResiduals(imageX.size(), 2);
+    std::cout<<"Residuals:"<<std::endl;
+    for (int n = 0; n<imageX.size(); n++)
+    {
+        imageResiduals(n,0) = residuals[2*n] * imageXStdDev[n];
+        imageResiduals(n,1) = residuals[2*n+1] * imageYStdDev[n];
+    }
+    std::cout<<imageResiduals<<std::endl;
 
     PyRun_SimpleString("print 'building Ceres-Solver cost functions:', round(TIME.clock()-t0, 3), 's' ");
 

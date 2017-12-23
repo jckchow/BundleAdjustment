ba40d6c928748a4bce0cbaebf6b326d4e410ef98
Jacky Chow <jckchow@ucalgary.ca>
2017-12-22 18:04:40 -0700

    Added cost function for collinearity

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,150 @@ struct constantConstraint {
   const double weight_;
 };
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Collinearity Equation
+/// Input:    x       - x observation
+///           y       - y observation
+///           xStdDev - noise
+///           yStdDev - noise
+/// Unknowns: x      - some unknown parameter in the adjustment
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct collinearity {
+  
+  collinearity(double x, double y, double xStdDev, double yStdDev, double xp, double yp)
+        : x_(x), y_(y), xStdDev_(xStdDev), yStdDev_(yStdDev), xp_(xp), yp_(yp)  {}
+
+  template <typename T>
+  // unknown parameters followed by the output residual
+  bool operator()(const T* const EOP, const T* const XYZ, const T* const IOP, const T* const AP, T* residual) const {
+
+  // rotation from map to sensor
+  T r11 = cos(EOP[1]) * cos(EOP[2]);
+  T r12 = cos(EOP[0]) * sin(EOP[2]) + sin(EOP[0]) * sin(EOP[1]) * cos(EOP[2]);
+  T r13 = sin(EOP[0]) * sin(EOP[2]) - cos(EOP[0]) * sin(EOP[1]) * cos(EOP[2]);
+
+  T r21 = -cos(EOP[1]) * sin(EOP[2]);
+  T r22 = cos(EOP[0]) * cos(EOP[2]) - sin(EOP[0]) * sin(EOP[1]) * sin(EOP[2]);
+  T r23 = sin(EOP[0]) * cos(EOP[2]) + cos(EOP[0]) * sin(EOP[1]) * sin(EOP[2]);
+
+  T r31 = sin(EOP[1]);
+  T r32 = -sin(EOP[0]) * cos(EOP[1]);
+  T r33 = cos(EOP[0]) * cos(EOP[1]);
+
+  // rigid body transformation
+  T XTemp = r11 * ( XYZ[0] - EOP[3] ) + r12 * ( XYZ[1] - EOP[4] ) + r13 * ( XYZ[2] - EOP[5] );
+  T YTemp = r21 * ( XYZ[0] - EOP[3] ) + r22 * ( XYZ[1] - EOP[4] ) + r23 * ( XYZ[2] - EOP[5] );
+  T ZTemp = r31 * ( XYZ[0] - EOP[3] ) + r32 * ( XYZ[1] - EOP[4] ) + r33 * ( XYZ[2] - EOP[5] );
+
+  // collinearity condition
+  T x = -IOP[2] * XTemp / ZTemp;
+  T y = -IOP[2] * YTemp / ZTemp;
+
+  // camera correction model AP = a1, a2, k1, k2, k3, p1, p2, ...
+  T x_bar = T(x_) - T(xp_);
+  T y_bar = T(y_) - T(yp_);
+  T r = sqrt(x_bar*x_bar + y_bar*y_bar);
+
+  T delta_x = x_bar*(AP[2]*pow(r,2.0)+AP[3]*pow(r,4.0)+AP[4]*pow(r,6.0)) + AP[5]*(pow(r,2.0)+2*pow(x_bar,2.0))+2.0*AP[6]*x_bar*y_bar + AP[0]*x_bar+AP[1]*y_bar;
+  T delta_y = y_bar*(AP[2]*pow(r,2.0)+AP[3]*pow(r,4.0)+AP[4]*pow(r,6.0)) + AP[6]*(pow(r,2.0)+2*pow(y_bar,2.0))+2.0*AP[5]*x_bar*y_bar;
+
+  T x_true = x + IOP[0] + delta_x;
+  T y_true = y + IOP[1] + delta_y;
+
+  // actual cost function
+  residual[0] = x_true - T(x_); // x-residual
+  residual[1] = y_true - T(y_); // y-residual    
+
+  residual[0] *= T(xStdDev_);
+  residual[1] *= T(yStdDev_);
+
+  return true;
+  }
+
+ private:
+  const double x_;
+  const double y_;
+  const double xStdDev_;
+  const double yStdDev_;
+  const double xp_;
+  const double yp_;
+
+};
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct lidarResidual3DCartesianRelative {
+  lidarResidual3DCartesianRelative(double x, double y, double z, double weight)
+      : x_(x), y_(y), z_(z), weight_(weight) {}
+
+  template <typename T>
+  // unknown parameters followed by the output residual
+  bool operator()(const T* const deltaPose, const T* const roll, const T* const pitch, const T* const yaw, const T* const Tx, const T* const Ty, const T* const Tz, const T* const XYZ, T* residual) const {
+
+//   std::cout<<"deltaPose: "<<deltaPose[0]<<", "<<deltaPose[1]<<", "<<deltaPose[2]<<", "<<deltaPose[3]<<", "<<deltaPose[4]<<", "<<deltaPose[5]<<std::endl;
+
+  // rotation from map to sensor
+  T r11 = cos(pitch[0]) * cos(yaw[0]);
+  T r12 = cos(roll[0]) * sin(yaw[0]) + sin(roll[0]) * sin(pitch[0]) * cos(yaw[0]);
+  T r13 = sin(roll[0]) * sin(yaw[0]) - cos(roll[0]) * sin(pitch[0]) * cos(yaw[0]);
+
+  T r21 = -cos(pitch[0]) * sin(yaw[0]);
+  T r22 = cos(roll[0]) * cos(yaw[0]) - sin(roll[0]) * sin(pitch[0]) * sin(yaw[0]);
+  T r23 = sin(roll[0]) * cos(yaw[0]) + cos(roll[0]) * sin(pitch[0]) * sin(yaw[0]);
+
+  T r31 = sin(pitch[0]);
+  T r32 = -sin(roll[0]) * cos(pitch[0]);
+  T r33 = cos(roll[0]) * cos(pitch[0]);
+
+  // EOP
+  T XTemp = r11 * ( XYZ[0] - Tx[0] ) + r12 * ( XYZ[1] - Ty[0] ) + r13 * ( XYZ[2] - Tz[0] );
+  T YTemp = r21 * ( XYZ[0] - Tx[0] ) + r22 * ( XYZ[1] - Ty[0] ) + r23 * ( XYZ[2] - Tz[0] );
+  T ZTemp = r31 * ( XYZ[0] - Tx[0] ) + r32 * ( XYZ[1] - Ty[0] ) + r33 * ( XYZ[2] - Tz[0] );
+
+  // boresight rotation matrix
+  T m11 = cos(deltaPose[1]) * cos(deltaPose[2]);
+  T m12 = cos(deltaPose[0]) * sin(deltaPose[2]) + sin(deltaPose[0]) * sin(deltaPose[1]) * cos(deltaPose[2]);
+  T m13 = sin(deltaPose[0]) * sin(deltaPose[2]) - cos(deltaPose[0]) * sin(deltaPose[1]) * cos(deltaPose[2]);
+
+  T m21 = -cos(deltaPose[1]) * sin(deltaPose[2]);
+  T m22 = cos(deltaPose[0]) * cos(deltaPose[2]) - sin(deltaPose[0]) * sin(deltaPose[1]) * sin(deltaPose[2]);
+  T m23 = sin(deltaPose[0]) * cos(deltaPose[2]) + cos(deltaPose[0]) * sin(deltaPose[1]) * sin(deltaPose[2]);
+
+  T m31 = sin(deltaPose[1]);
+  T m32 = -sin(deltaPose[0]) * cos(deltaPose[1]);
+  T m33 = cos(deltaPose[0]) * cos(deltaPose[1]);
+
+  // boresight and leverarm
+  T x_true =  m11 * ( XTemp - deltaPose[3] ) + m12 * ( YTemp - deltaPose[4] ) + m13 * ( ZTemp - deltaPose[5] );
+  T y_true =  m21 * ( XTemp - deltaPose[3] ) + m22 * ( YTemp - deltaPose[4] ) + m23 * ( ZTemp - deltaPose[5] );
+  T z_true =  m31 * ( XTemp - deltaPose[3] ) + m32 * ( YTemp - deltaPose[4] ) + m33 * ( ZTemp - deltaPose[5] );
+
+  residual[0] = x_true - T(x_); // x-residual
+  residual[1] = y_true - T(y_); // y-residual        
+  residual[2] = z_true - T(z_); // z-residual, should be zero if we have a 2D LiDAR
+
+//   std::cout<<"residual: "<<residual[0]<<", "<<residual[1]<<", "<<residual[2]<<std::endl;
+//   sleep(1000000);
+
+  residual[0] *= T(weight_);
+  residual[1] *= T(weight_);
+  residual[2] *= T(1/1.0E-6);
+
+  return true;
+  }
+
+ private:
+  // Observations for a sample.
+  const double x_;
+  const double y_;
+  const double z_;
+  const double weight_;
+};
+
+
+
 /////////////////////////
 /// MAIN CERES-SOLVER ///
 /////////////////////////
